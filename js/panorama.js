e3ffd6d91767e97f27471f3223a02b8bc5b1b21e
Maurice Heumann <maurice.heumann@wibu.com>
2020-11-30 18:54:35 +0100

    Chrome best browser ever.

--- js/panorama.js
+++ js/panorama.js
@@ -20,21 +20,17 @@
     });
 
     function init() {
-
         const container = document.getElementById('panorama');
 
         camera = new THREE.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 1, 1100);
-
         scene = new THREE.Scene();
 
         const geometry = new THREE.SphereBufferGeometry(500, 60, 40);
         // invert the geometry on the x-axis so that all of the faces point inward
         geometry.scale(-1, 1, 1);
 
-        const material = new THREE.MeshBasicMaterial({ map: texture });
-
+        const material = new THREE.MeshBasicMaterial({ map: texture })
         const mesh = new THREE.Mesh(geometry, material);
-
         scene.add(mesh);
 
         renderer = new THREE.WebGLRenderer();
