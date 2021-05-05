116de305875e81a257e7526f9a9e26df00d650b5
Chase Hall <c@chse.xyz>
2021-05-05 10:40:32 -0500

    CRLF line endings.

--- _footer.js
+++ _footer.js
@@ -1,1 +1,24 @@
-// If you are adding new lines, make sure to escape the line at the end with a \document.write('\    <footer>\        <div class="container">\            <div class="row">\                <div class="col-md-12">\                    <div class="social-icons wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="0.3s">\                        <ul>\                            <li class="twitter"><a href="https://twitter.com/XLabsProject"><i class="fab fa-twitter"></i></a></li>\                            <li class="discord"><a href="https://discord.gg/sKeVmR3"><i class="fab fa-discord"></i></a></li>\                            <li class="youtube"><a href="https://www.youtube.com/channel/UCPA9X79ZsCXMBBCSJ63wA3A?sub_confirmation=1"><i class="fab fa-youtube"></i></a></li>\                            <li class="github"><a href="https://github.com/XLabsProject"><i class="fab fa-github"></i></a></li>\                            <li class="patreon"><a href="https://www.patreon.com/xlabsproject"><i class="fab fa-patreon"></i></a></li>\                        </ul>\                    </div>\                    <div class="site-info wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="0.3s">\                        <p>All copyrights reserved &copy; 2021 - <a rel="nofollow" href="https://xlabs.dev">X Labs</a></p>\                    </div>\                </div>\            </div>\        </div>\    </footer>\');
+// If you are adding new lines, make sure to escape the line at the end with a \
+
+document.write('\
+    <footer>\
+        <div class="container">\
+            <div class="row">\
+                <div class="col-md-12">\
+                    <div class="social-icons wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="0.3s">\
+                        <ul>\
+                            <li class="twitter"><a href="https://twitter.com/XLabsProject"><i class="fab fa-twitter"></i></a></li>\
+                            <li class="discord"><a href="https://discord.gg/sKeVmR3"><i class="fab fa-discord"></i></a></li>\
+                            <li class="youtube"><a href="https://www.youtube.com/channel/UCPA9X79ZsCXMBBCSJ63wA3A?sub_confirmation=1"><i class="fab fa-youtube"></i></a></li>\
+                            <li class="github"><a href="https://github.com/XLabsProject"><i class="fab fa-github"></i></a></li>\
+                            <li class="patreon"><a href="https://www.patreon.com/xlabsproject"><i class="fab fa-patreon"></i></a></li>\
+                        </ul>\
+                    </div>\
+                    <div class="site-info wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="0.3s">\
+                        <p>All copyrights reserved &copy; 2021 - <a rel="nofollow" href="https://xlabs.dev">X Labs</a></p>\
+                    </div>\
+                </div>\
+            </div>\
+        </div>\
+    </footer>\
+');
