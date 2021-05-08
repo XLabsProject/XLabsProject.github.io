5431e8a1d803c81a6bba8352f44ef381f2756f13
Maurice Heumann <maurice.heumann@wibu.com>
2021-05-08 13:33:36 +0200

    Use target blank

--- footer.js
+++ footer.js
@@ -7,11 +7,11 @@ document.write('\
                 <div class="col-md-12">\
                     <div class="social-icons wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="0.3s">\
                         <ul>\
-                            <li class="twitter"><a href="https://twitter.com/XLabsProject"><i class="fab fa-twitter"></i></a></li>\
-                            <li class="discord"><a href="https://discord.gg/sKeVmR3"><i class="fab fa-discord"></i></a></li>\
-                            <li class="youtube"><a href="https://www.youtube.com/channel/UCPA9X79ZsCXMBBCSJ63wA3A?sub_confirmation=1"><i class="fab fa-youtube"></i></a></li>\
-                            <li class="github"><a href="https://github.com/XLabsProject"><i class="fab fa-github"></i></a></li>\
-                            <li class="patreon"><a href="https://www.patreon.com/xlabsproject"><i class="fab fa-patreon"></i></a></li>\
+                            <li class="twitter"><a target="_blank" href="https://twitter.com/XLabsProject"><i class="fab fa-twitter"></i></a></li>\
+                            <li class="discord"><a target="_blank" href="https://discord.gg/sKeVmR3"><i class="fab fa-discord"></i></a></li>\
+                            <li class="youtube"><a target="_blank" href="https://www.youtube.com/channel/UCPA9X79ZsCXMBBCSJ63wA3A?sub_confirmation=1"><i class="fab fa-youtube"></i></a></li>\
+                            <li class="github"><a target="_blank" href="https://github.com/XLabsProject"><i class="fab fa-github"></i></a></li>\
+                            <li class="patreon"><a target="_blank" href="https://www.patreon.com/xlabsproject"><i class="fab fa-patreon"></i></a></li>\
                         </ul>\
                     </div>\
                     <div class="site-info wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="0.3s">\
